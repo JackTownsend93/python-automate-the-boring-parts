6607e4ae5b2feacff20f6a2577dfd0734adb4fcb
jack.townsend <johndoe@example.com>
2022-02-14 17:46:55 +0000

    environment setup and basic test

---
+++ p01-c01-python-basics/01-hello-world.py
@@ -0,0 +1,7 @@
+# Basic hello world and user str input.
+print('Hello, world!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
