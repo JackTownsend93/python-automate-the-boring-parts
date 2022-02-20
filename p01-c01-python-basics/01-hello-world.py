90449bfefefcef01f0501a88f10a02e74b85396f
JackTownsend93 <townsend.jack1993@gmail.com>
2022-02-20 15:29:08 +0000

    changing dir structure

--- p01-c01-python-basics/01-hello-world.py
+++
@@ -1,7 +0,0 @@
-# Basic hello world and user str input.
-print('Hello, world!')
-print('What is your name?')
-myName = input()
-print('It is good to meet you, ' + myName)
-print('The length of your name is: ')
-print(len(myName))
