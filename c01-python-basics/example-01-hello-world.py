e581bf37ff73666d1a7361e6426649c5fbd0db65
JackTownsend93 <townsend.jack1993@gmail.com>
2022-02-20 15:26:44 +0000

    change dir structure and completed chapter 1

---
+++ c01-python-basics/example-01-hello-world.py
@@ -0,0 +1,10 @@
+# Basic hello world and user str input.
+print('Hello, world!')
+print('What is your name?') # Prompt user for name.
+myName = input()
+print('It is good to meet you, ' + myName + '.')
+print('The length of your name is: ')
+print(len(myName))
+print('What is your age?') # Prompt user for age.
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
